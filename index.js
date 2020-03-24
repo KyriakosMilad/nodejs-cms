2cd4e557e14ddf12f7dd29452d5996ae8eec2acc
Kyriakos Milad <kero.cool16@gmail.com>
2020-03-24 11:25:44 +0200

    setting up views and essentials

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.engine(
+	'hbs',
+	hbs({
+		extname: 'hbs',
+		defaultLayout: 'main',
+		layoutsDir: __dirname + '/views/layouts'
+	})
+);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+	res.render('home/index', { title: 'hello there' });
+});
