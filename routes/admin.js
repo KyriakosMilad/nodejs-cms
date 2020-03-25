5e9ab3dd891820e36aa5e670b38ed6debc303e82
Kyriakos Milad <kero.cool16@gmail.com>
2020-03-25 09:44:49 +0200

    adding email uniqe custom validation to create user

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { check, body } = require('express-validator');
 
+const User = require('../models/User');
+
 const adminController = require('../controllers/adminController');
 const adminUsersController = require('../controllers/adminUsersController');
 
@@ -13,13 +15,20 @@ router.get('/admin/users/create', adminUsersController.getCreateUser);
 router.post(
 	'/admin/users/create',
 	check('name')
-  .isLength({ min: 3, max: 18 })
-  .withMessage('name field must be 3 letters min and 18 max'),
+		.isLength({ min: 3, max: 18 })
+		.withMessage('name field must be 3 letters min and 18 max'),
 	check('email')
 		.isEmail()
 		.withMessage('make sure entering a valid email'),
+	body('email').custom(val => {
+		return User.findOne({ where: { email: val } }).then(user => {
+			if (user) {
+				return Promise.reject('E-mail already in use');
+			}
+		});
+	}),
 	check('password')
-  .isLength({ min: 5, max: 64 })
+		.isLength({ min: 5, max: 64 })
 		.withMessage('Password must be 5 digits min and 64 max'),
 	adminUsersController.createUser
 );
