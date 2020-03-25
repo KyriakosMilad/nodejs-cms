892e5244ed05c58596a98ff0ea4273093476f3f8
Kyriakos Milad <kero.cool16@gmail.com>
2020-03-25 13:53:05 +0200

    working on confirmtion modal

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,32 +1,51 @@
-function confirmModel(formID, text, type) {
+function confirmModal(formID, text, type) {
+	// check inputs
+	if (!formID || !text || !type) {
+		return console.log('Error: params required');
+	}
+
 	// Get the modal
 	const confModal = document.getElementById('confModal');
 
 	// Get the button that opens the modal
-	const modalSubmit = document.getElementById('modalSubmit');
+	const modalSubmit = document.getElementById('confModalSubmit');
 
 	// Get the <span> element that closes the modal
-	const modalClose = document.getElementById('modalClose');
+	const modalClose = document.getElementById('confModalClose');
 
-	// When the user clicks on the button, open the modal
+	// Get the <p> element that contains the modal text
+	const modalText = document.getElementById('confModalText');
+
+	modalText.textContent = text;
+
+	// check type and change button class and text
+	if (type === 1) {
+		modalSubmit.className = 'btn-primary';
+		modalSubmit.textContent = 'SUBMIT';
+	}
+
+	if (type === 2) {
+		modalSubmit.className = 'btn-danger';
+		modalSubmit.textContent = 'DELETE';
+	}
+
+	// open the modal
 	confModal.style.display = 'block';
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
+	// close the modal
+	modalClose.addEventListener('click', function() {
 		confModal.style.display = 'none';
-	};
+	});
 
-	if (!formID || !text || !type) {
-		return console.log('Error: params required');
-	}
-	if (type === 0) {
-	} else if (type === 1) {
-	}
+	// submit the modal from related
+	modalSubmit.addEventListener('click', function() {
+		document.getElementById(formID).submit();
+	});
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.addEventListener('click', function(event) {
+		if (event.target == confModal) {
+			confModal.style.display = 'none';
+		}
+	});
 }
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == confModal) {
-//     confModal.style.display = 'none';
-//   }
-// };
